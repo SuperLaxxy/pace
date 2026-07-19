99f651f020beb4cc6546a6b0ddfcf2b02f6e042f
SuperLaxxy <naxa88408@gmail.com>
2026-07-20 05:41:54 +0900

    Update envelope.js

--- client/src/crypto/envelope.js
+++ client/src/crypto/envelope.js
@@ -1,4 +1,4 @@
-import "/src/crypto-engine/serialize.js";
+import "../crypto-engine/serialize.js";
 
 export const ENVELOPE_FIELDS = globalThis.serializeModule.ENVELOPE_FIELDS;
 export const serializeEnvelope = globalThis.serializeModule.serializeEnvelope;
